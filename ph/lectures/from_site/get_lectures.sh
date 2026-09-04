40ad17d49a82f16308bdfd5ae9e59c41a34de1f4
solovmikebmstu <solovmikebmstu@gmail.com>
2026-09-04 16:10:06 +0300

    add ph lectures

---
+++ ph/lectures/from_site/get_lectures.sh
@@ -0,0 +1,15 @@
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_01_02.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_03.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_04.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_05_06.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_07_08.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_09.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_10.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_11.pdf
+wget http://fn.bmstu.ru/images/FN4/phys-educ-work/lectures/sem3_lectures_12_13.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_14_15.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_16.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_lec_17.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_pril_01.pdf
+wget https://fn.bmstu.ru/files/FN4/lec_3sem/3sem_pril_02.pdf
+
